b46e03bf77f39a6d942e8f2bfd88bfc2738e36a3
Bharath <143115318+bharathsvri@users.noreply.github.com>
2024-09-16 17:10:42 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header() {
   return (
     <HeaderContainer className="container">
       <h4 data-aos="fade-down">
-        <a href="https://bharathsvri-portfolio.netlify.app">Bharath </a>
+        <a href="https://bharathsvri.netlify.app">Bharath </a>
       </h4>
     </HeaderContainer>
   );
